4ad766168ac2eda77ccab2dfde286eb2ac69ca8d
Juan Scarton <jscarton@gmail.com>
2019-05-06 16:15:28 -0300

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
  * Welcome route - link to any public API documentation here
  */
 Route::get('/', function () {
-    echo 'Welcome to our API';
+    echo 'Welcome to ArepasApp API';
 });
 
 /**
@@ -29,48 +29,6 @@ Route::get('/', function () {
  */
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', ['middleware' => ['api']], function ($api) {
-    /*
-     * Authentication
-     */
-    $api->group(['prefix' => 'auth'], function ($api) {
-        $api->group(['prefix' => 'jwt'], function ($api) {
-            $api->get('/token', 'App\Http\Controllers\Auth\AuthController@token');
-        });
-    });
-
-    /*
-     * Authenticated routes
-     */
-    $api->group(['middleware' => ['api.auth']], function ($api) {
-        /*
-         * Authentication
-         */
-        $api->group(['prefix' => 'auth'], function ($api) {
-            $api->group(['prefix' => 'jwt'], function ($api) {
-                $api->get('/refresh', 'App\Http\Controllers\Auth\AuthController@refresh');
-                $api->delete('/token', 'App\Http\Controllers\Auth\AuthController@logout');
-            });
-
-            $api->get('/me', 'App\Http\Controllers\Auth\AuthController@getUser');
-        });
-
-        /*
-         * Users
-         */
-        $api->group(['prefix' => 'users', 'middleware' => 'check_role:admin'], function ($api) {
-            $api->get('/', 'App\Http\Controllers\UserController@getAll');
-            $api->get('/{uuid}', 'App\Http\Controllers\UserController@get');
-            $api->post('/', 'App\Http\Controllers\UserController@post');
-            $api->put('/{uuid}', 'App\Http\Controllers\UserController@put');
-            $api->patch('/{uuid}', 'App\Http\Controllers\UserController@patch');
-            $api->delete('/{uuid}', 'App\Http\Controllers\UserController@delete');
-        });
-
-        /*
-         * Roles
-         */
-        $api->group(['prefix' => 'roles'], function ($api) {
-            $api->get('/', 'App\Http\Controllers\RoleController@getAll');
-        });
-    });
+    $api->get('xoom/{country}', 'App\Http\Controllers\XoomController@rate');
+});
 });
